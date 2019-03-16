0a9be185e24fe227f6bc40286f0a101a247fb131
Jesse Perdue <perdue.jesse@gmail.com>
2019-03-15 19:18:56 -0700

    Messing with the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,26 @@ import React from "react";
 class Contact extends React.Component {
   render() {
     return (
-      <form netlify name="contact" method="post">
-        <input type="text" />
-        <button>Submit</button>
+      <form name="contact" method="post">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
     );
   }
