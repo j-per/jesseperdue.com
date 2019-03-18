85380e0e3fbc5c91d31e322375e121791a364cd5
Jesse Perdue <perdue.jesse@gmail.com>
2019-03-17 20:52:58 -0700

    changed HREF values back in the footer to hash

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,19 +9,16 @@ class Footer extends React.Component {
         <Container className="FooterContent_Container my-5">
           <Row>
             <Col>
-              <a href="https://github.com/j-per" target="_blank">
+              <a href="#" target="_blank">
                 <i className="fab fa-github fa-2x FooterContent_icon" />
               </a>
-              <a href="https://codepen.io/jpound/" target="_blank">
+              <a href="#" target="_blank">
                 <i className="fab fa-codepen fa-2x FooterContent_icon" />
               </a>
-              <a href="https://twitter.com/jesse90perdue" target="_blank">
+              <a href="#" target="_blank">
                 <i className="fab fa-twitter fa-2x FooterContent_icon" />
               </a>
-              <a
-                href="https://www.linkedin.com/in/jesse-perdue-1aa8a996/"
-                target="_blank"
-              >
+              <a href="#" target="_blank">
                 <i className="fab fa-linkedin fa-2x FooterContent_icon" />
               </a>
             </Col>
